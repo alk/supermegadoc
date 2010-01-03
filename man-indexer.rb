92294f6c8521a88cef9b78eba039ae4dbb9988dd
Aliaksey Kandratsenka <alk@tut.by>
2010-01-03 21:12:40 +0200

    fixed non-digit sections in  man-indexer.rb and added whatis support

--- man-indexer.rb
+++ man-indexer.rb
@@ -10,13 +10,20 @@ def process_dir(dir)
 
   hash = {}
   files.map do |path|
-    next unless path =~ /(?:\A|\/)man(\d+)\/(.+)\.(\d+)(\.(gz|bz2))?\z/
-    next unless $1 == $3
+    raise path unless path =~ /(?:\A|\/)man(\d)\/(.+)\.(\d[^\.]*)(\.(gz|bz2))?\z/
+    raise path unless $1 == $3[0,1]
     next unless File.readable?(path)
-    section, name = $1, $2
+    section, name = $3, $2
     hash["#{section}/#{name}"] = path
   end
   hash
+end
+
+$lexgrog = false
+
+if `which lexgrog`.size != 0
+  STDERR.puts "lexgrog is present. Will add whatis info"
+  $lexgrog = true
 end
 
 hashes = ARGV.map(&self.method(:process_dir))
@@ -24,8 +31,41 @@ hash = hashes.reverse.inject({}) do |r,h|
   r.merge(h)
 end
 
+if $lexgrog
+  new_hash = {}
+  processed_files = {}
+  hash.each do |k,v|
+    next if processed_files[v]
+    processed_files[v] = true
+    lines = IO.popen("-") do |f|
+      if f # parent
+        f.read
+      else # child
+        # this allows me to avoid shell-quoting v
+        exec "lexgrog", "-w", v
+      end
+    end.split("\n")
+    re = /\A#{Regexp.escape(v)}:\s+"(.+)"\s*\z/
+    lines.each do |l|
+      unless l =~ re
+        STDERR.puts "!#{l}"
+        new_hash[k] = [k,v]
+        next
+      end
+      line = $1
+      section, = k.split('/', 2)
+      new_hash["#{section}/#{line}"] = [k,v]
+    end
+  end
+  hash = new_hash
+end
+
 hash.each do |k,v|
-  print_cdb_entry k, "man-file:#{v}\0man:#{k}"
+  if Array === v
+    print_cdb_entry k, "man-file:#{v[1]}\0man:#{v[0]}"
+  else
+    print_cdb_entry k, "man-file:#{v}\0man:#{k}"
+  end
 end
 
 puts
