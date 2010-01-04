a619261274e148c2b5390882cfe4cc58213d32c4
Aliaksey Kandratsenka <alk@tut.by>
2010-01-04 18:20:50 +0200

    fixed indexing non-ascii entries on ruby 1.9

--- common.rb
+++ common.rb
@@ -1,6 +1,11 @@
+unless String.instance_methods.include?(:bytesize)
+  class String
+    alias_method :bytesize, :size
+  end
+end
 
 module Kernel
   def print_cdb_entry(key, value)
-    print "+#{key.size},#{value.size}:#{key}->#{value}\n"
+    print "+#{key.bytesize},#{value.bytesize}:#{key}->#{value}\n"
   end
 end
