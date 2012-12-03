d52be4c920a645236e72f8851c2f147e06d80917
Aliaksey Kandratsenka <alk@tut.by>
2012-12-03 14:02:50 -0800

    stolen ri indexing code from yari

--- ri-indexer.rb
+++ ri-indexer.rb
@@ -8,13 +8,23 @@ def print_ri(v)
   print_cdb_entry(v, "ri:#{v}")
 end
 
-IO.popen("ri --list", "r") do |f|
-  f.each_line do |l|
-    l.chomp!
-    print_ri(l)
+IO.popen("-", "r") do |f|
+  unless f
+    # child
+    require 'rdoc/ri/driver'
+    driver  = RDoc::RI::Driver.new
+    puts driver.list_known_classes
+    puts driver.list_methods_matching('.')
+    exit
+  else
+    # parent
+    f.each_line do |l|
+      l.chomp!
+      next if l.empty?
+      print_ri(l)
+    end
   end
 end
-
 
 print_cdb_entry "--extra-args", "--dir-separator='#'"
 
